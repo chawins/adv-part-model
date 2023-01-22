a1dfb677a25df7b7e0bcd443fa3abc591411cfef
Nabeel Hingun <nab_126@red5k.CS.Berkeley.EDU>
2023-01-21 17:31:14 -0800

    added prep bbox for part imagenet

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -11,7 +11,7 @@
 # source /global/home/users/$USER/.bash_profile
 # module purge
 # module load python
-source activate /global/scratch/users/$USER/env_part_based
+# source activate /global/scratch/users/$USER/env_part_based
 
 # python prepare_cityscapes.py \
 #     --seed 0 \
@@ -29,7 +29,30 @@ source activate /global/scratch/users/$USER/env_part_based
 #     --name aeroplane_bird_car_cat_dog_new \
 #     --min-area 0.
 
-python -u prepare_part_imagenet.py \
-    --data-dir /data/nab_126/PartImageNet/ \
-    --name All
-# # --use-box-seg
+# python -u prepare_part_imagenet.py \
+#     --data-dir /data/nab_126/PartImageNet/ \
+#     --name All
+
+
+DATAPATH=/data1/chawins/PartImageNet 
+
+# ### Data Prep
+# mkdir $DATAPATH/PartBoxSegmentations/train/
+# mkdir $DATAPATH/PartBoxSegmentations/val/
+# mkdir $DATAPATH/PartBoxSegmentations/test/
+
+# ln -s $DATAPATH/JPEGImages/* $DATAPATH/PartBoxSegmentations/train/
+# ln -s $DATAPATH/JPEGImages/* $DATAPATH/PartBoxSegmentations/val/
+# ln -s $DATAPATH/JPEGImages/* $DATAPATH/PartBoxSegmentations/test/
+
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split train
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split val
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split test
+
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split train --use-imagenet-classes
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split val --use-imagenet-classes
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split test --use-imagenet-classes
+
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split train --group-parts
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split val --group-parts
+# python prepare_part_imagenet_bbox.py --label-dir $DATAPATH --split test --group-parts
