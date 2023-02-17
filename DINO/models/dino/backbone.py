115b76ccd507bbe60095a77499485967f818a2a3
Chawin Sitawarin <chawins@berkeley.edu>
2023-02-17 13:53:35 -0800

    Allow specifying batchnorm type from options

--- DINO/models/dino/backbone.py
+++ DINO/models/dino/backbone.py
@@ -115,12 +115,9 @@ class Backbone(BackboneBase):
                  batch_norm=FrozenBatchNorm2d,
                  ):
         if name in ['resnet18', 'resnet34', 'resnet50', 'resnet101']:
-            # backbone = getattr(torchvision.models, name)(
-            #     replace_stride_with_dilation=[False, False, dilation],
-            #     pretrained=is_main_process(), norm_layer=batch_norm)
             backbone = getattr(torchvision.models, name)(
                 replace_stride_with_dilation=[False, False, dilation],
-                pretrained=is_main_process())
+                pretrained=is_main_process(), norm_layer=batch_norm)
         else:
             raise NotImplementedError("Why you can get here with name {}".format(name))
         # num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
--- part_model/models/det_part_models/dino_bbox_model.py
+++ part_model/models/det_part_models/dino_bbox_model.py
@@ -8,11 +8,8 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from DINO.models.dino.dino import (
-    DINO,
-    build_backbone,
-    build_deformable_transformer,
-)
+from DINO.models.dino.backbone import Backbone, Joiner, build_position_encoding
+from DINO.models.dino.dino import DINO, build_deformable_transformer
 from DINO.util.misc import NestedTensor
 from part_model.utils.types import BatchImages, Logits
 
@@ -33,7 +30,7 @@ class DinoBoundingBoxModel(nn.Module):
         # Temporary model options
         self._use_sigmoid = "sig" in args.experiment
 
-        backbone = build_backbone(args)
+        backbone = _build_backbone(args)
         transformer = build_deformable_transformer(args)
 
         # TODO(nab-126@): Remove the try/except block. Try to never use
@@ -155,3 +152,48 @@ class DinoBoundingBoxModel(nn.Module):
         if return_mask:
             return out, dino_outputs
         return out
+
+
+def _build_backbone(args):
+    """Build backbone for DINO. Modified from DINO/models/dino/backbone.py.
+
+    Useful args:
+        - backbone: backbone name
+        - lr_backbone:
+        - dilation
+        - return_interm_indices: available: [0,1,2,3], [1,2,3], [3]
+        - backbone_freeze_keywords:
+        - use_checkpoint: for swin only for now
+    """
+    position_embedding = build_position_encoding(args)
+    # This can be removed
+    train_backbone = args.lr_backbone > 0
+    if not train_backbone:
+        raise ValueError("Please set lr_backbone > 0")
+    return_interm_indices = args.return_interm_indices
+    assert return_interm_indices in [[0, 1, 2, 3], [1, 2, 3], [3]]
+
+    if args.backbone in ["resnet50", "resnet101"]:
+        backbone = Backbone(
+            args.backbone,
+            train_backbone,
+            args.dilation,
+            return_interm_indices,
+            batch_norm=getattr(torch.nn, args.batch_norm_type),
+        )
+        bb_num_channels = backbone.num_channels
+    else:
+        raise NotImplementedError(
+            "Only resnet50 and resnet101 are supported for now!"
+        )
+    assert len(bb_num_channels) == len(return_interm_indices), (
+        f"len(bb_num_channels) {len(bb_num_channels)} != "
+        f"len(return_interm_indices) {len(return_interm_indices)}"
+    )
+
+    model = Joiner(backbone, position_embedding)
+    model.num_channels = bb_num_channels
+    assert isinstance(
+        bb_num_channels, list
+    ), f"bb_num_channels is expected to be a List but {type(bb_num_channels)}!"
+    return model
